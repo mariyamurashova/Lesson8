46015956cfdb8f0ed3e7ddc12a797e19d3040e51
mariyamurashova <127765865+mariyamurashova@users.noreply.github.com>
2023-04-14 15:59:16 +0200

    Create passenger_train.rb

---
+++ passenger_train.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class PassengerTrain < Train
+  PassengerTrain.set_instances
+  PassengerTrain.set_register_instances
+
+  def initialize(name)
+    super
+    @train_type = :passenger
+  end
+end
